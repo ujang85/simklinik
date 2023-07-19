a604db9fed86a63fddac10bd06aed0316ad59b94
ujang85 <litvinenco85@gmail.com>
2023-07-19 10:01:36 +0700

    Tugas-11-Firebase-CRUD

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMYU_piGPiWHjArWpJ2Wk9jUYT9ibJ0eI",
+  authDomain: "simklinik-55f95.firebaseapp.com",
+  databaseURL: "https://simklinik-55f95-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "simklinik-55f95",
+  storageBucket: "simklinik-55f95.appspot.com",
+  messagingSenderId: "1052329734331",
+  appId: "1:1052329734331:web:d3d768241936bf49dc399e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <main>
+    <h1>Halaman muka</h1>
+    
+    <div class="row">
+      <form>
+        <div class="form-group">
+          <div class="col-md-12">
+            <div class="mb-3">
+                <label  class="form-label"> Title</label>
+                <input type="text">
+            </div>
+        </div>
+        </div>
+
+        <div class="form-group">
+          <div class="col-md-12">
+            <div class="mb-3">
+                <label  class="form-label"> <span class="text-danger">Content</span></label>
+                <input type="textarea">
+            </div>
+        </div>
+        </div>
+        
+      </form>
+    </div>
+    
+  </main>
+</template>
